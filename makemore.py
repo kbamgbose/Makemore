74d2c0973341f6ed4383175cafccd067a5227290
kbamgbose <kbamgbose@gmail.com>
2026-01-31 11:09:55 -0800

    Neural Net Makemore

--- makemore.py
+++ makemore.py
@@ -1,5 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 
 words = open('names.txt', 'r').read().splitlines()
@@ -17,7 +18,7 @@ sorted(b.items(), key = lambda kv: -kv[-1])
 N = torch.zeros((27, 27), dtype=torch.int32)
 
 chars = sorted(list(set(''.join(words))))
-stoi = {s:i+1 for i, s in enumerate(chars)}
+stoi = {s:i +1 for i, s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s, i in stoi.items()}
 
@@ -39,12 +40,86 @@ for i in range(27):
         plt.text(j, i, N[i, j].item(), ha="center", va="top", color='gray')
 plt.axis('off');
 
-print(N[0, :])
-
+g = torch.Generator().manual_seed(2147483647)
 p = N[0].float()
+p = p / p.sum()
+ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g)
+
+N[0]
+p = torch.rand(3, generator=g)
 p = p / p.sum()
 
 g = torch.Generator().manual_seed(2147483647)
-p = torch.rand(3, generator=g)
-print(p)
-# p = p / p.sum()
+
+out = []
+ix = 0;
+
+P = (N+1).float()
+P /= P.sum(1, keepdims=True)
+
+for i in range(5):
+
+    out = []
+    ix = 0;
+
+    while True:
+        p = P[ix]
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+        if ix == 0:
+            break
+    print(''.join(out))
+
+P.sum(1, keepdim=True).shape
+
+
+# GOAL: maximize likelihood of the data w.r.t model parameters (statistical modeling)
+# equivalent to maximizing the log likelihood (because log is monotonic)
+# equivalent to minimzing the negative log likelihood
+# equivalent to minimizing the average negative log likelihood
+
+# log(a*b*c) = log(a) + log(b) + log(c)
+log_likelihood = 0.0
+n = 0
+
+for w in words[:3]:
+# for w in ["kola"]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+        # print(f'{ch1}{ch2}: {prob:.4f} {logprob:.4f}')
+
+# print(log_likelihood)
+# nll = -log_likelihood
+# print(f'{nll=}')
+# print(f'{nll/n}')
+
+
+# create the training set of all the bigrams(x, y)
+xs, ys = [], []
+
+for w in words[:1]:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        print(ch1, ch2)
+        xs.append(ix1)
+        ys.append(ix2)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+xenc = F.one_hot(xs, num_classes=27).float()
+
+W = torch.randn((27, 27))
+# (5, 27) @ (27, 1)
+logits = xenc @ W # log-counts
+counts = (xenc @ W).exp() #equivalent N
+probs = counts / counts.sum(1, keepdims=True)
+print(probs)
