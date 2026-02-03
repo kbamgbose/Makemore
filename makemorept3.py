9e57136e71d01ad024e5a67d4ee2c4e6b0310c06
kbamgbose <kbamgbose@gmail.com>
2026-02-02 20:11:20 -0800

    Makemore pt 3 final

--- makemorept3.py
+++ makemorept3.py
@@ -57,6 +57,8 @@ W2 = torch.randn((n_hidden, vocab_size), generator=g) * 0.01
 b2 = torch.randn(vocab_size, generator=g) * 0
 bngain = torch.ones((1, n_hidden))
 bnbias = torch.zeros((1, n_hidden))
+bnmean_running = torch.zeros((1, n_hidden))
+bnstd_running = torch.ones((1, n_hidden))
 
 
 
@@ -66,7 +68,7 @@ for p in parameters:
     p.requires_grad = True
 
 # same optimization as last time
-max_steps = 200000
+max_steps =  100 #200000
 batch_size = 32
 lossi = []
 
@@ -79,10 +81,19 @@ for i in range(max_steps):
     #forward pass
     emb = C[Xb] # embed the characters into vectors
     embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
-    hpreact = embcat @ W1 + b1 # hidden layer pre-activation
+    # Linear layer
+    hpreact = embcat @ W1 # + b1 # hidden layer pre-activation
+    # BatchNorm layer
+    # -------------------------------------------------------------------
     bnmeani = hpreact.std(0, keepdim=True)
     bnstdi = hpreact.std(0, keepdim=True)
     hpreact = bngain * (hpreact - hpreact.mean(0, keepdim=True)) / hpreact.std(0, keepdim=True) + bnbias
+
+    with torch.no_grad():
+        bnmean_running = 0.999 * bnmean_running + 0.001 * bnmeani
+        bnstd_running = 0.99 * bnstd_running + 0.001 * bnstdi
+    # --------------------------------------------------------------------
+    # Non linearity
     h = torch.tanh(hpreact) # hidden layer
     logits = h @ W2 + b2 # output layer
     loss = F.cross_entropy(logits, Yb) # loss function
@@ -95,7 +106,8 @@ for i in range(max_steps):
     #update
     lr = 0.1 if i < 100000 else 0.01 #step learning rate decay
     for p in parameters:
-        p.data += -lr * p.grad
+        if p.grad is not None:
+            p.data += -lr * p.grad
     
     #track stats
     if i % 10000 == 0: # print every once in a while
@@ -122,7 +134,7 @@ def split_loss(split):
     emb = C[x] # (N, block_size, n_embd)
     embcat = emb.view(emb.shape[0], -1) # concat into (N, block_size * n_embd)
     hpreact = embcat @ W1 + b1
-    hpreact = bngain * (hpreact - bnmean) / bnstd + bnbias
+    hpreact = bngain * (hpreact - bnmean_running) / bnstd_running + bnbias
     h = torch.tanh(embcat @ W1 + b1) # (N, n_hidden)
     logits = h @ W2 + b2 # (N, vocab_size)
     loss = F.cross_entropy(logits, y)
@@ -154,4 +166,131 @@ for _ in range(20):
         
     print(''.join(itos[i] for i in out)) # decode and print the generated word
 
+
+#----------------------------- SUMMARY-------------------------------------
+
+# Let's train a deeper network
+
+class Linear:
+
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out), generator=g) / fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+
+    def __call__(self, x):
+        self.out = x @ self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+    
+    def parameters(self):
+        return [self.weight] + ([] if self.bias is None else [self.bias])
+
+class BatchNorm1d:
+
+    def __init__(self, dim, eps=1e-5, momentum=0.1):
+        self.eps = eps
+        self.momentum = momentum
+        self.training = True
+        #parameters (trained with backprop)
+        self.gamma = torch.ones(dim)
+        self.beta = torch.zeros(dim)
+        # buffers (trained with a running 'momentum update')
+        self.running_mean = torch.zeros(dim)
+        self.running_var = torch.ones(dim)
+    
+    def __call__(self, x):
+        # calculate the forward pass
+        if self.training:
+            xmean = x.mean(0, keepdim=True) #batch mean
+            xvar = x.var(0, keepdim=True) #batch variance
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps) #normalize to unit variance
+        self.out = self.gamma * xhat + self.beta
+        #update the buffers
+        if self.training:
+            with torch.no_grad():
+                self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * xmean
+                self.running_var = (1 - self.momentum) * self.running_var + self.momentum * xvar
+            return self.out
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+class Tanh:
+    def __call__(self, x):
+        self.out = torch.tanh(x)
+        return self.out
+    def parameters(self):
+        return []
+
+n_embd = 10 # the dimensionality of the character embedding vectors
+n_hidden = 100 # the number of neurons in the hidden layer of the MLP
+g = torch.Generator().manual_seed(2147483647) # for reporducibility
+
+C = torch.rand((vocab_size, n_embd), generator=g)
+layers = [
+    Linear(n_embd * block_size, n_hidden), BatchNorm1d(n_hidden),  Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden),Tanh(),
+    Linear(n_hidden, n_hidden), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, vocab_size), BatchNorm1d(vocab_size),
+]
+
+with torch.no_grad():
+    #last layer: make less confident
+    layers[-1].gamma *= 0.1
+    # all other lyaers: apply gain
+    for layer in layers[:-1]:
+        if isinstance(layer, Linear):
+            layer.weight *= 5/3
+
+parameters = [C] + [p for layer in layers for p in layer.parameters()]
+print(sum(p.nelement() for p in parameters)) # number of parameters in total
+for p in parameters:
+    p.requires_grad = True
+
+
+# same optimization as last time
+max_steps = 100 #200000
+batch_size = 32
+lossi = []
+ud = []
+
+for i in range(max_steps):
+
+    #minibatch construct
+    ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+    Xb, Yb = Xtr[ix], Ytr[ix] # batch X, Y
+
+    #forward pass
+    emb = C[Xb] # embed the characters into vectors
+    x = emb.view(emb.shape[0], -1)
+    for layer in layers:
+        x = layer(x)
+    loss = F.cross_entropy(x, Yb) # loss function
+  
+    # backward pass
+    for layer in layers:
+        layer.out.retain_grad() # AFTER_DEBUG: would take out retain_graph
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+
+    #update
+    lr = 0.1 if i < 100000 else 0.01 #step learning rate decay
+    for p in parameters:
+        if p.grad is not None:
+            p.data += -lr * p.grad
+    
+    #track stats
+    if i % 10000 == 0: # print every once in a while
+        print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
+    lossi.append(loss.log10().item())
+    with torch.no_grad():
+        ud.append([(lr*p.grad.std() / p.data.std()).log10().item() for p in parameters])
+
+    break # AFTER_DEBUG: would take out obviously to run full optimization
 
